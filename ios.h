30a466946c15d97305560c436bf5723be56dc892
asmaa-badreldin <participant@Ahmeds-Mac.local>
2016-04-11 16:27:15 +0200

    testing add new file

---
+++ ios.h
@@ -0,0 +1,13 @@
+//
+//  ios.h
+//  IOSProject
+//
+//  Created by JETS on 4/11/16.
+//  Copyright (c) 2016 ITI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ios <NSObject>
+
+@end
